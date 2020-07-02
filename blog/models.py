e099b7cca4e9aa6a8571bf7114a3dec4d9546893
Atiq <rehman652@gmail.com>
2020-07-03 01:44:47 +0500

    project ready for VPS

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,12 @@ class Blog (models.Model):
     sub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='')
+
+    def __str__ (self):
+        return self.title
+
+    def summary (self):
+        return self.body[:100]
+
+    def sub_date_short (self):
+        return self.sub_date.strftime('%b %e %Y')
